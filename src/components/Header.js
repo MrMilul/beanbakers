4d9f7c9729db9e13b8c9f8631c3abb2e242f348a
mrmilul <miladmizani@yahoo.com>
2022-02-02 13:38:00 +0200

    simple Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = ()=> {
+  return (
+    <nav className="navbar navbar-light bg-dark">
+      <a className="navbar-brand" href="#">
+        <img src="https://beanbakers.fi/img/BeanBakers_horizontal_white.86f02de8.svg"  alt=""/>
+      </a>
+    </nav>
+  );
+}
+
+export default Header;
